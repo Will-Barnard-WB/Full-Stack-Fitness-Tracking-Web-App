51968652f69f271aeca45faa3c8376b02ccb8570
etiennevdeynde <elvde20@bath.ac.uk>
2025-04-05 10:48:57 +0200

    Turned "Split" into a record class
    Since "Split" only holds primitives, it can be a record class.

--- DataBase/Serializers/Split.java
+++ DataBase/Serializers/Split.java
@@ -1,54 +1,7 @@
-public class Split {
+package Serializers;
 
-    private final int timeStamp;
-    private final long positionLong;
-    private final long positionLat;
-    private final long distance;
-    private final long altitude;
-    private final long speed;
-    private final int cadence;
-    private final int heartRate;
+import java.io.Serializable;
 
-    public Split(int timeStamp, long positionLong, long positionLat, long distance, long altitude, long speed, int cadence, int heartRate) {
-        this.timeStamp = timeStamp;
-        this.positionLong = positionLong;
-        this.positionLat = positionLat;
-        this.distance = distance;
-        this.altitude = altitude;
-        this.speed = speed;
-        this.cadence = cadence;
-        this.heartRate = heartRate;
-    }
-
-    public int getCadence() {
-        return cadence;
-    }
-
-    public int getHeartRate() {
-        return heartRate;
-    }
-
-    public long getDistance() {
-        return distance;
-    }
-
-    public int getTimeStamp() {
-        return timeStamp;
-    }
-
-    public long getAltitude() {
-        return altitude;
-    }
-
-    public long getPositionLat() {
-        return positionLat;
-    }
-
-    public long getPositionLong() {
-        return positionLong;
-    }
-
-    public long getSpeed() {
-        return speed;
-    }
+public record Split(int timeStamp, long positionLong, long positionLat, long distance, long altitude, long speed,
+                    int cadence, int heartRate) implements Serializable {
 }
