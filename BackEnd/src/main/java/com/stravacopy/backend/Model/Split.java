1b718efadac10925f5e25532990ce189e2da267c
Marcel Lanecki <mdlanecki@icloud.com>
2025-04-07 13:51:15 +0100

    Add new classes for Mood, Split, User, Workout, and UserRepository

---
+++ BackEnd/src/main/java/com/stravacopy/backend/Model/Split.java
@@ -0,0 +1,20 @@
+package com.stravacopy.backend.Model;
+
+import java.time.LocalDateTime;
+
+public class Split {
+
+    private LocalDateTime timeStamp;
+    private Integer Longitude;
+    private Integer Latitude;
+    private Integer Altitude;
+    private Integer Distance;
+    private Integer Speed;
+    private Integer Cadence;
+    private Integer HeartRate;
+
+
+
+
+    // Add further attributes as needed
+}
