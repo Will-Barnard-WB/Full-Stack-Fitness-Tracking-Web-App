bb3e96f31ba684b8e1c05274a967efb48b8c48ca
HarryThacker <153201304+HarryThacks@users.noreply.github.com>
2025-04-19 13:41:55 +0100

    Update SignIn.js

--- FrontEnd/src/pages/SignIn.js
+++ FrontEnd/src/pages/SignIn.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './SignInAndGraphs.css';
 
 function SignIn() {
     const navigate = useNavigate();
