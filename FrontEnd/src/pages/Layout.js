42bfbf3adece93cc1604a4f3093a828cfb3e0d05
Brandon2408 <blgreenstone@gmail.com>
2025-04-22 18:09:53 +0100

    Update Layout.js

--- FrontEnd/src/pages/Layout.js
+++ FrontEnd/src/pages/Layout.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
     return (
         <div>
-                <Outlet /> {/* This is where child routes will be rendered */}
+            <Outlet /> {/* This is where child routes will be rendered */}
         </div>
     );
 }
