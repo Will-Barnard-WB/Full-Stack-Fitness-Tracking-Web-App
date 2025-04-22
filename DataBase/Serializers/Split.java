3c87f2adb639d61665f32fcfc530dfa7fc4fe7f9
etiennevdeynde <elvde20@bath.ac.uk>
2025-04-22 14:09:50 +0100

    Update Split.java

--- DataBase/Serializers/Split.java
+++ DataBase/Serializers/Split.java
@@ -2,6 +2,6 @@ package Serializers;
 
 import java.io.Serializable;
 
-public record Split(int timeStamp, long positionLong, long positionLat, long distance, long altitude, long speed,
+public record Split(long timeStamp, long positionLong, long positionLat, long distance, long altitude, long speed,
                     int cadence, int heartRate) implements Serializable {
 }
